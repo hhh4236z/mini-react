3c6142780829b8793b40ea63d104f94b7526dd55
kkopite <kkopitehong@foxmail.com>
2024-03-27 21:54:41 +0800

    feat(day4): 动态更新props

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
 import React from './core/React.js'
 
+let count = 10
+let props = {
+  id: 1234
+}
 function Counter({ num }) {
   function handleClick() {
     console.log('click')
+    count++
+    props = {}
+    React.update()
   }
-  return (<div>This is {num}
+  return (<div>This is {count}
     <button onClick={handleClick}>click</button>  
   </div>);
 }
 
 function App() {
   return (
-  <div>
-    hello
-    <Counter num={10}/>
-    <Counter num={20}/>
+  <div {...props}>
+    <Counter />
   </div>    
   )
 }
--- core/React.js
+++ core/React.js
@@ -1,13 +1,13 @@
 const TEXT_ELEMENT = 'TEXT ELEMENT'
 
-// 调度
-let nextFiber = null
-// 统一提交
-let root = null
+let nextWorkOfUnit = null
+// work in progress
+let wipRoot = null
+// 记录旧的节点
+let currentRoot = null
 
 function render(node, container) {
-  // 构造一个根节点的fiber
-  nextFiber = {
+  wipRoot = {
     dom: container,
     props: {
       // 这样 parent.dom 就能找到 container 来挂载
@@ -15,7 +15,18 @@ function render(node, container) {
     },
   }
 
-  root = nextFiber
+  nextWorkOfUnit = wipRoot
+  requestIdleCallback(loop)
+}
+
+function update() {
+  wipRoot = {
+    dom: currentRoot.dom,
+    props: currentRoot.props,
+    alternate: currentRoot,
+  }
+
+  nextWorkOfUnit = wipRoot
   requestIdleCallback(loop)
 }
 
@@ -50,31 +61,74 @@ function createDom(type) {
     : document.createElement(type)
 }
 
-function updateProps(dom, props) {
-  Object.keys(props).forEach((key) => {
+function updateProps(dom, newProps, oldProps = {}) {
+  // 1. 旧的节点有，新的没有，删除即可
+  Object.keys(oldProps).forEach((key) => {
+    if (key !== 'children') {
+      if (!(key in newProps))
+        dom.removeAttribute(key)
+    }
+  })
+
+  // 2. 新的节点与旧节点有差异
+
+  Object.keys(newProps).forEach((key) => {
+    if ((newProps[key] === oldProps[key])) {
+      // 不变的话，就跳过即可！
+      return
+    }
     if (key.startsWith('on')) {
       const eventType = key.slice(2).toLowerCase()
-      dom.addEventListener(eventType, props[key])
+      // 卸载旧的
+      dom.removeEventListener(eventType, oldProps[key])
+
+      dom.addEventListener(eventType, newProps[key])
     }
     else if (key !== 'children') {
-      dom[key] = props[key]
+      // 更新
+      dom[key] = newProps[key]
     }
   })
 }
 
-function initChildren(fiber, children) {
+function reconcileChildren(fiber, children) {
   // 处理子节点
   let prevFiber = null
+  // 拿出旧的对应fiber
+  // 第一个要比较的显然就是旧的fiber的child
+  let oldFiber = fiber.alternate?.child
   children.forEach((child, index) => {
-    // 构造一个，而不是直接在 child本身添加
-    // child 是一个 vdom
-    const newFiber = {
-      type: child.type,
-      props: child.props,
-      parent: fiber,
-      dom: null,
-      sibiling: null,
-      child: null,
+    let newFiber
+
+    const isSameType = oldFiber?.type === child.type
+
+    if (isSameType) {
+      // 相同type，那就只是更新，不用挂载了
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        parent: fiber,
+        // 还是原来的 dom，不需要更新
+        dom: oldFiber.dom,
+        sibiling: null,
+        child: null,
+        // 记录上对应的旧节点
+        alternate: oldFiber,
+        effectTag: 'update',
+      }
+    }
+    else {
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        parent: fiber,
+        dom: null,
+        sibiling: null,
+        child: null,
+        // 记录上对应的旧节点
+        alternate: oldFiber,
+        effectTag: 'placement',
+      }
     }
 
     if (index === 0) {
@@ -87,13 +141,16 @@ function initChildren(fiber, children) {
     }
 
     prevFiber = newFiber
+
+    // 旧节点对应更新
+    oldFiber = oldFiber?.sibiling
   })
 }
 
 function updateFunctionComponent(fiber) {
   // fiber.type 即为函数式组件的函数，传入props
   const children = [fiber.type(fiber.props)]
-  initChildren(fiber, children)
+  reconcileChildren(fiber, children)
 }
 
 function updateHoistComponent(fiber) {
@@ -103,7 +160,7 @@ function updateHoistComponent(fiber) {
 
     updateProps(el, fiber.props)
   }
-  initChildren(fiber, fiber.props.children)
+  reconcileChildren(fiber, fiber.props.children)
 }
 
 // 处理单个任务
@@ -140,23 +197,25 @@ function performUnitWork(fiber) {
 function loop(deadline) {
   let shouldYield = false
 
-  while (!shouldYield && nextFiber) {
-    nextFiber = performUnitWork(nextFiber)
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performUnitWork(nextWorkOfUnit)
     shouldYield = deadline.timeRemaining() < 1
   }
 
-  if (!nextFiber && root) {
+  if (!nextWorkOfUnit && wipRoot) {
     // 没有下一个 fiber 了，说明递归结束了，可以提交了
     // 统一挂载，避免有些任务先挂载显示了，然后后续的没有空闲时间，等会就才挂载
     commitRoot()
-    root = null
+    // 保存好旧的节点
+    currentRoot = wipRoot
+    wipRoot = null
   }
   requestIdleCallback(loop)
 }
 
 function commitRoot() {
   // 根的 dom已经是挂载的
-  commitFiber(root.child)
+  commitFiber(wipRoot.child)
 }
 
 function commitFiber(fiber) {
@@ -168,8 +227,12 @@ function commitFiber(fiber) {
   while (!parentFiber.dom)
     parentFiber = parentFiber.parent
 
-  if (fiber.dom)
-    parentFiber.dom.append(fiber.dom)
+  if (fiber.dom) {
+    if (fiber.effectTag === 'update')
+      updateProps(fiber.dom, fiber.props, fiber.alternate.props)
+    else if (fiber.effectTag === 'placement')
+      parentFiber.dom.append(fiber.dom)
+  }
 
   commitFiber(fiber.child)
   commitFiber(fiber.sibiling)
@@ -177,6 +240,7 @@ function commitFiber(fiber) {
 
 const React = {
   render,
+  update,
   createElement,
 }
 
